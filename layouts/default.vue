8cf4e19ca5926fe8cfda016f2821e95f25392e3a
Lo <l709937065@gmail.com>
2024-10-10 16:55:46 +0800

    chore: init

---
+++ layouts/default.vue
@@ -0,0 +1,5 @@
+<template>
+  <section class="font-mono">
+    <slot />
+  </section>
+</template>
---
+++ uno.config.ts
@@ -0,0 +1,23 @@
+import {
+  defineConfig,
+  presetIcons,
+  presetUno,
+  presetWebFonts,
+  transformerDirectives,
+  transformerVariantGroup,
+} from "unocss"
+
+export default defineConfig({
+  presets: [
+    presetUno(),
+    presetIcons(),
+    presetWebFonts({
+      fonts: {
+        sans: "DM Sans",
+        serif: "DM Serif Display",
+        mono: "DM Mono",
+      },
+    }),
+  ],
+  transformers: [transformerDirectives(), transformerVariantGroup()],
+})
