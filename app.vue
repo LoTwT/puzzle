8e2b564223164cebcebda5d6788e3677a5693588
Lo <l709937065@gmail.com>
2024-10-16 17:47:18 +0800

    chore: update title and description

--- app.vue
+++ app.vue
@@ -1,3 +1,16 @@
+<script setup lang="ts">
+useHead({
+  title: "Puzzle",
+})
+
+useSeoMeta({
+  title: "Puzzle",
+  ogTitle: "Puzzle",
+  description: "A puzzle game",
+  ogDescription: "A puzzle game",
+})
+</script>
+
 <template>
   <NuxtRouteAnnouncer />
   <NuxtLayout>
