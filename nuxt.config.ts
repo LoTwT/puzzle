5db4ab53e481d844730ec34352cd93e75c34dcbf
Lo <l709937065@gmail.com>
2024-10-22 13:55:36 +0800

    chore: update compatibilityDate

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-04-03",
+  compatibilityDate: "2024-10-22",
+
   devtools: { enabled: true },
+
   modules: ["@unocss/nuxt", "@vueuse/nuxt"],
+
   css: ["@unocss/reset/tailwind-compat.css"],
+
   vite: {
     css: {
       preprocessorOptions: {
